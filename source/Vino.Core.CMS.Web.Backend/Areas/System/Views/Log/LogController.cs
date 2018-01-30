3a1ef1af0e3a9cd6ef48f2f701f062f6f715c10c
kulend <kulend@qq.com>
2018-01-30 14:21:20 +0800

    none

---
+++ source/Vino.Core.CMS.Web.Backend/Areas/System/Views/Log/LogController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Vino.Core.CMS.Service.System;
+using Vino.Core.CMS.Web.Base;
+using Vino.Core.CMS.Web.Security;
+using Vino.Core.Infrastructure.Exceptions;
+
+namespace Vino.Core.CMS.Web.Admin.Areas.System.Views.Log
+{
+    [Area("System")]
+    [Auth("sys.log")]
+    public class LogController : BackendController
+    {
+        private IUserActionLogService service;
+
+        public LogController(IUserActionLogService service)
+        {
+            this.service = service;
+        }
+
+        [Auth("view")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Auth("view")]
+        public async Task<IActionResult> GetList(int page, int rows)
+        {
+            var data = await service.GetListAsync(page, rows);
+            return PagerData(data.items, page, rows, data.count);
+        }
+
+
+        [Auth("view")]
+        public async Task<IActionResult> Detail(long id)
+        {
+            var module = await service.GetByIdAsync(id);
+            if (module == null)
+            {
+                throw new VinoDataNotFoundException("无法取得数据!");
+            }
+            return View(module);
+        }
+    }
+}

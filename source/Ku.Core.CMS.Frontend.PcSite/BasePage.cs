6a7c290f4e195982295d4d804bb24c178e970c04
kulend <kulend@qq.com>
2018-08-14 23:17:57 +0800

    add pcsite

---
+++ source/Ku.Core.CMS.Frontend.PcSite/BasePage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ku.Core.CMS.Frontend.PcSite
+{
+    public class BasePage : PageModel
+    {
+
+    }
+
+    /// <summary>
+    /// 需要登陆的页面
+    /// </summary>
+    [Authorize]
+    public class LoginAuthPage : BasePage
+    {
+
+    }
+}
